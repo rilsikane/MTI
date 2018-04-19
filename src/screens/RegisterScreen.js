1768dec2aa64e0ed7adb3111643d984e581c85a7
Rilsikane <jutha.se@gmail.com>
2018-04-19 10:32:06 +0700

    no message

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -176,13 +176,23 @@ export default class RegisterScreen extends Component{
             let response = await postBasic("mti/checkinfo",param,true);
             this.app.isLoading = false;
             if(response){
-                if(!response.message){
+                if(!response.message && "checkMember"!=response.data_source){
                     this.props.registerStore.register = {...response};
                     this.props.registerStore.register.idcard = param.idcard;
                     this.props.registerStore.register.birthdate = param.birthdate;
                     this.setState({enable:true});
                     this._pages.scrollToPage(1);
                     this.setState({enable:false});
+                }else{
+                    this.props.navigator.resetTo({
+                        screen: 'mti.LoginScreen', // unique ID registered with Navigation.registerScreen
+                        title: undefined, // navigation bar title of the pushed screen (optional)
+                        titleImage: undefined, // iOS only. navigation bar title image instead of the title text of the pushed screen (optional)
+                        passProps: {}, // Object that will be passed as props to the pushed screen (optional)
+                        animated: true, // does the push have transition animation or does it happen immediately (optional)
+                        backButtonTitle: undefined, // override the back button title (optional)
+                        backButtonHidden: false, // hide the back button altogether (optional)
+                    })
                 }
             }else{
                 this.popupDialog.show();
