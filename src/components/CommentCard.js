61432110f746fa122491211ff4851e102cb5da8c
AFaLook <despair_me15@hotmail.com>
2018-04-25 17:03:06 +0700

    Init ActivityDetailScreen.

---
+++ src/components/CommentCard.js
@@ -0,0 +1,176 @@
+import React,{Component} from 'react';
+import {Text,View,Image,TouchableOpacity,FlatList} from 'react-native';
+import {Item,Input} from 'native-base';
+import PropTypes from "prop-types";
+import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
+
+class CommentCard extends Component{
+
+    constructor(props){
+        super(props)
+
+    }
+
+    renderCommentList(){
+        return(
+            <FlatList
+                data={this.props.commentList}
+                keyExtractor={this._keyExtractor}
+                renderItem={this._renderItem}
+            />
+       )
+    }
+
+    _renderItem = ({item}) => (
+            <View style={styles.commentListSectionStyle}>
+                <View style={styles.userThumbnailContainerStyle}>
+                    <Image
+                        source={item.userThumbUri}
+                        style={styles.userThumbnailStyle}
+                    />
+                </View>
+                <View style={styles.commentDetailContainerStyle}>
+                    <View style={styles.commentTitleContainerStyle}>
+                        <Text style={styles.userNameTextStyle}>{item.userName}</Text>
+                        <Text style={styles.commentDateTextStyle}>{item.createDate}</Text>
+                    </View>
+                    <Text style={styles.commentDetailTextStyle}>{item.commentDetail}</Text>
+                    <Image
+                        source={require('../source/images/dotSectionHorizontal.png')}
+                        resizeMode='contain'
+                        style={styles.commentDotSectionBottomImageStyle}
+                    />
+                </View>
+            </View> 
+    );
+
+    _keyExtractor = (item, index) => index.toString();
+
+    render(){
+        return(
+            <View style={styles.commentContainerStyle}>
+                <Image
+                    source={require('../source/images/dotSectionHorizontal.png')}
+                    resizeMode='contain'
+                    style={styles.dotSectionBottomImageStyle}
+                />
+                <View style={styles.totalCommentContainerStyle}>
+                    <Image
+                        source={require('../source/icons/iconCommentSelected.png')}
+                        resizeMode='contain'
+                        style={styles.commentIconStyle}
+                    />
+                    <Text style={styles.totalCommentTextStyle}>{`ความคิดเห็น [ ${this.props.commentList.length} ]`}</Text>
+                </View>
+                <View style={styles.commentListContainerStyle}>
+                    {this.renderCommentList()}
+                    <View style={[styles.commentListSectionStyle,styles.userInputContainerStyle]}>
+                        <View style={styles.userThumbnailContainerStyle}>
+                            <Image
+                                source={require('../source/images/userAvatarImg.png')}
+                                style={styles.userThumbnailStyle}
+                            />
+                        </View>
+                        <Item style={styles.commentInputContainerStyle}>
+                            <Input 
+                                placeholder='ความคิดเห็นของคุณ...'
+                                placeholderTextColor='rgba(145, 145, 149, 0.44)'
+                            />
+                            <TouchableOpacity onPress={this.props.onSendMessagePress}>
+                                <Image
+                                    source={require('../source/icons/iconSendMessage.png')}
+                                    resizeMode='contain'
+                                    style={styles.sendMessageIconStyle}
+                                />
+                            </TouchableOpacity>
+                        </Item>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles={
+    commentContainerStyle:{
+      
+    },
+    dotSectionBottomImageStyle:{
+        width: '100%',
+        opacity: 0.3,
+        marginTop: responsiveHeight(2.5),
+        marginBottom: responsiveHeight(1),
+    },
+    totalCommentContainerStyle:{
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    commentIconStyle:{
+        height: responsiveHeight(2.81),
+        width: responsiveWidth(5.15),
+        marginRight: responsiveWidth(1.5),
+    },
+    totalCommentTextStyle:{
+        letterSpacing: 0,
+        color: "#1595d3",
+        fontSize: responsiveFontSize(2.8)
+    },
+    commentListContainerStyle:{
+        marginTop: responsiveHeight(1),
+    },
+    commentListSectionStyle:{
+        flexDirection: 'row',
+    },
+    userThumbnailContainerStyle:{
+        height: responsiveHeight(3.96),
+        flex: 0.1,
+    },
+    userThumbnailStyle:{
+        height: responsiveHeight(3.96),
+        width: responsiveHeight(3.96),
+        borderRadius: responsiveHeight(1.98),
+        borderWidth: 1,
+        borderColor: "rgba(168, 168, 170, 0.5)",
+        marginTop: responsiveHeight(0.5),
+    },
+    commentDetailContainerStyle:{
+        flex: 0.9,
+    },
+    commentTitleContainerStyle:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    userNameTextStyle:{
+        letterSpacing: 0,
+        color: "#1595d3",
+        fontSize: responsiveFontSize(2.2),
+    },
+    commentDateTextStyle:{
+        letterSpacing: 0,
+        color: "rgba(145, 145, 149, 0.69)",
+        fontSize: responsiveFontSize(2),
+    },
+    commentDetailTextStyle:{
+        letterSpacing: 0,
+        color: "#919195",
+        fontSize: responsiveFontSize(2.15),
+    },
+    commentDotSectionBottomImageStyle:{
+        width: '100%',
+        opacity: 0.3,
+        marginTop: responsiveHeight(1),
+        marginBottom: responsiveHeight(1),
+    },
+    commentInputContainerStyle:{
+        flex: 0.9,
+        height: responsiveHeight(4.1)
+    },
+    sendMessageIconStyle:{
+        height: responsiveHeight(2.81),
+    },
+    userInputContainerStyle:{
+        marginTop: responsiveHeight(1),
+    }
+}
+
+export {CommentCard}
---
+++ src/screens/ActivityDetailScreen.js
@@ -0,0 +1,206 @@
+import React,{Component} from 'react';
+import {Text,View,Image,ScrollView,TouchableOpacity} from 'react-native';
+import PropTypes from "prop-types";
+import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
+
+import {Headers} from '../components/Headers';
+import {EventButtonGroup} from '../components/EventButtonGroup';
+import {MainSubmitButton} from '../components/MainSubmitButton';
+import {CommentCard} from '../components/CommentCard';
+import {PastEventCard} from '../components/PastEventCard';
+
+export default class ActivityDetailScreen extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            showComment: true,
+        }
+    }
+
+    renderOtherActivityList(){
+        let event = [
+            {
+                bannerUri: require('../source/images/activityImg01.png'),
+                eventTitleText: 'MTI 8 Anniversary "ยิ้มรับความสำเร็จ..ฉลอง ก้าวแห่งความภาคภูมิใจ" กับเมืองไทยประกันภัย',
+                eventDetailText: 'นำลูกค้าล่องเรือชมบรรยากาศริมแม่น้ำเจ้าพระยา พร้อมรับประทานอาหารค่ำและชมมินิคอนเสิร์ต จากศิลปินคู่ ดูโอ แอน(ธิติมา) - ปิงปอง(ศิรศักดิ์) พร้อมกันนี้ ยังมีกิจกรรม...'
+            },
+            {
+                bannerUri: require('../source/images/latestActImg.png'),
+                eventTitleText: 'MTI 8 Anniversary "ยิ้มรับความสำเร็จ..ฉลอง ก้าวแห่งความภาคภูมิใจ" กับเมืองไทยประกันภัย',
+                eventDetailText: 'นำลูกค้าล่องเรือชมบรรยากาศริมแม่น้ำเจ้าพระยา พร้อมรับประทานอาหารค่ำและชมมินิคอนเสิร์ต จากศิลปินคู่ ดูโอ แอน(ธิติมา) - ปิงปอง(ศิรศักดิ์) พร้อมกันนี้ ยังมีกิจกรรม...'
+            }
+        ]
+
+        return event.map((data,i)=>
+            <PastEventCard
+                key={i}
+                bannerUri={data.bannerUri}
+                eventTitleText={data.eventTitleText}
+                eventDetailText={data.eventDetailText}
+                style={[{marginLeft: responsiveWidth(3)},i==event.length-1&&{marginRight: responsiveWidth(3)}]}
+            />
+        )
+    }
+
+    render(){
+        let comment = [
+            {
+                userThumbUri: require('../source/images/userCommentThumb01.png'),
+                userName: 'ภิรยา',
+                createDate: '5 มีนาคม 2061',
+                commentDetail: 'กิจกรรมนี้สนุกมากเลยค่ะ เป็นกันเองสุดๆเลย อยากให้จัดแบบนี้บ่อยๆ จังเลยค่ะ'
+            },
+        ]
+
+        return(
+            <View style={styles.ActivityDetailScreenContainerStyle}>
+                <Headers
+                    leftIconName='back'
+                    headerTitleText='รายละเอียดกิจกรรม'
+                />
+                <ScrollView style={{flex: 1,}}>
+                    <View style={styles.activityDetailContainerStyle}>
+                        <View style={styles.activityBannerImageContainerStyle}>
+                            <Image
+                                source={require('../source/images/latestActImg.png')}
+                                //resizeMode='stretch'
+                                borderRadius={3}
+                                style={styles.bannerImageStyle}
+                            />
+                        </View>
+                        <View style={styles.activityContentContainerStyle}>
+                            <Text style={styles.activityTitleTextStyle}>MTI 8 Anniversary "ยิ้มรับความสำเร็จ..ฉลอง ก้าวแห่งความภาคภูมิใจ"</Text>
+                            <Text style={styles.activityDescriptionTextStyle}>เมืองไทยประกันภัยเชิญลูกค้า ร่วมกิจกรรม"เฉลิมฉลองครบรอบ 8 ปี พร้อมก้าวสู่ปีที่ 9 กับเมืองไทยประกันภัย"  โดยนำลูกค้าล่องเรือชมบรรยากาศริมแม่น้ำเจ้าพระยา พร้อมรับประทานอาหารค่ำและชมมินิคอนเสิร์ต จากศิลปินคู่ดูโอ แอน(ธิติมา) - ปิงปอง(ศิรศักดิ์) พร้อมกันนี้ ยังมีกิจกรรมให้ร่วมสนุกกันอย่างสนุกสนานอีกด้วย</Text>
+                            <Image
+                                source={require('../source/images/dotSectionHorizontal.png')}
+                                resizeMode='contain'
+                                style={styles.dotSectionImageStyle}
+                            />
+                            <View style={styles.eventButtonGroupContainerStyle}>
+                                <Text style={styles.activityDateTextStyle}>วันที่ 12 กุมภาพันธ์ 2061</Text>
+                                <EventButtonGroup
+                                    isFavorite
+                                    isShareSelected
+                                />
+                            </View>
+                            <MainSubmitButton
+                                buttonTitleText='กรอกแบบสอบถามกิจกรรม'
+                                onPress={()=>alert('submit')}
+                                style={styles.submitButtonStyle}
+                            />
+                            {this.state.showComment&&
+                            <CommentCard
+                                commentList={comment}
+                                onSendMessagePress={()=>alert('send')}
+                            />}
+                        </View>
+                    </View>
+                    <View style={styles.otherActivityContainerStyle}>
+                        <View style={styles.otherActivityTitleContainerStyle}>
+                            <Text style={styles.otherActivityTitleTextStyle}>กิจกรรมอื่น</Text>
+                            <TouchableOpacity>
+                                <Text style={styles.seeAllTextStyle}>ดูทั้งหมด</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.otherActivityListContainerStyle}>
+                            <ScrollView horizontal style={{flex: 1,}} showsHorizontalScrollIndicator={false}>
+                                {this.renderOtherActivityList()}
+                            </ScrollView>
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
+        )
+    }
+}
+
+const styles={
+    ActivityDetailScreenContainerStyle:{
+        flex: 1,
+    },
+    activityDetailContainerStyle:{
+        flex: 1,
+        backgroundColor: '#f6f6f6',
+        borderBottomWidth: responsiveHeight(0.17),
+        borderColor: '#dddddd',
+        paddingBottom: responsiveHeight(2.5),
+    },
+    activityBannerImageContainerStyle:{
+        height: responsiveHeight(23.23),
+        width: responsiveWidth(90),
+        alignSelf: 'center',
+        marginTop: responsiveHeight(3.5),
+    },
+    bannerImageStyle:{
+        height: responsiveHeight(23.23),
+        width: responsiveWidth(90),
+    },
+    activityContentContainerStyle:{
+        marginLeft: responsiveWidth(5),
+        marginRight: responsiveWidth(5),
+    },
+    activityTitleTextStyle:{
+        fontFamily: "DBHelvethaicaX-Med",
+        fontSize: responsiveFontSize(3),
+        letterSpacing: 0,
+        color: "#1595d3",
+        marginTop: responsiveHeight(1),
+        marginBottom: responsiveHeight(1),
+    },
+    activityDescriptionTextStyle:{
+        fontSize: responsiveFontSize(2.15),
+        letterSpacing: 0.28,
+        color: "#919195"
+    },
+    dotSectionImageStyle:{
+        width: '100%',
+        opacity: 0.3,
+        marginTop: responsiveHeight(1),
+        marginBottom: responsiveHeight(2),
+        alignSelf: 'center',
+    },
+    dotSectionBottomImageStyle:{
+        width: '100%',
+        opacity: 0.3,
+        marginTop: responsiveHeight(2.5),
+        marginBottom: responsiveHeight(1),
+    },
+    eventButtonGroupContainerStyle:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    activityDateTextStyle:{
+        fontSize: responsiveFontSize(2.15),
+        letterSpacing: 0.2,
+        color: "#1595d3"
+    },
+    submitButtonStyle:{
+        marginTop: responsiveHeight(2),
+    },
+    otherActivityContainerStyle:{
+        flex: 1,
+    },
+    otherActivityTitleContainerStyle:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: responsiveWidth(4.6),
+        marginRight: responsiveWidth(4.6),
+        marginTop: responsiveHeight(2),
+        marginBottom: responsiveHeight(1.5),
+    },
+    otherActivityTitleTextStyle:{
+        letterSpacing: 0,
+        color: "#1595d3",
+        fontSize: responsiveFontSize(2.8),
+        fontFamily: "DBHelvethaicaX-Med",
+    },
+    seeAllTextStyle:{
+        color: "rgba(85, 86, 90, 0.6)",
+        fontSize: responsiveFontSize(2),
+    },
+    otherActivityListContainerStyle:{
+        flex: 1,
+        marginBottom: responsiveHeight(2),
+    }
+}
