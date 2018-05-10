8717ed5ad0aab2562b21805d90d00d55940a534f
Rilsikane <jutha.se@gmail.com>
2018-05-10 07:51:14 +0700

    no message

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -320,7 +320,7 @@ export default class LoginScreen extends Component{
                                     </TouchableOpacity>
                                     
                                 </View> */}
-                                <Text style={{color:"#fff"}}>Version : 1.48</Text>
+                                <Text style={{color:"#fff"}}>Version : 1.49</Text>
                                 <View style={styles.registerBottomContainerStyle}>
                                     <TouchableOpacity onPress={this.gotoRegister}>
                                         <Text style={styles.registerBottomTextStyle}>ลงทะเบียนสมาชิก</Text>
